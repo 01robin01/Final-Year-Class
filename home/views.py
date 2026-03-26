00fbd419e29f525e4082b5862ec8a439e45033f0
WotfRobin <chhetri.robin01@gmail.com>
2026-03-26 11:41:58 +0545

    added search page and its css and changes made in home view

--- home/views.py
+++ home/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render
 from .models import Item,ItemImage
 
 def index(request):
-    items = Item.objects.all().order_by('-id')[:10]
+    
+    items = (
+        Item.objects
+        .prefetch_related('images')
+        .order_by('-id')[:10]
+    )
+
+  
+    if not items:
+        items = [
+            {
+                "title": "Lost Wallet",
+                "location": "Kathmandu",
+                "status": "Lost",
+                "images": []
+            },
+            {
+                "title": "Found Phone",
+                "location": "Lalitpur",
+                "status": "Found",
+                "images": []
+            }
+        ]
+
+    context = {
+        "items": items
+    }
 
 
-    return render(request, 'Index.html', {'items': items})
+    return render(request, "Index.html", context)
 
 
